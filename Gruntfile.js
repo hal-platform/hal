8f59fc0aa3b37d8cdffa4756427c8ecb0f41472d
Dave Gillhespy <yodasw16@gmail.com>
2013-10-28 13:40:25 -0400

    initial frontend setup

---
+++ Gruntfile.js
@@ -0,0 +1,39 @@
+module.exports = function(grunt) {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+
+        // Watch Task
+        watch: {
+            sass: {
+                files: ['sass/**/*.scss'],
+                tasks: ['compass:dev'],
+            },
+        },
+
+        // Compass Task
+        compass: {
+            dev: {
+                options: {
+                    httpPath: 'public',
+                    cssDir: 'public/css',
+                    sassDir: 'sass',
+                    imagesDir: 'public/img',
+                    javascriptsDir: 'public/js',
+                    force: true,
+                    outputStyle: 'compact',
+                    relativeAssets: true,
+                    require: ['modular-scale'],
+                },
+            },
+            dist: {
+                options: {
+                    outputStyle: 'compressed',
+                },
+            },
+        },
+
+    });
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-compass');
+};
+
