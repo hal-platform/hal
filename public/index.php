919a53b84fecd34396aa9beb34dfcf018a4fe83a
Steve Kluck <stevekluck@quickenloans.com>
2014-04-17 09:51:20 -0400

    Move logger setup into a slim hook
    SERVER_ADDR is read from the global SERVER since slim environment does not provide it.

--- public/index.php
+++ public/index.php
@@ -16,12 +16,6 @@ require_once __DIR__.'/../app/bootstrap.php';
 $app = $container->get('slim');
 $app->view($container->get('twigView'));
 
-// Set required logging properties from the environment
-if ($serverIp = IPv4Address::create($app->environment()['SERVER_ADDR'])) {;
-    $container->get('mcpLogger.factory')->setDefaultProperty('machineIPAddress', $serverIp);
-}
-$container->get('mcpLogger.factory')->setDefaultProperty('machineName', $app->environment()['SERVER_NAME']);
-
 // 404 Error Handler
 $app->notFound(function () use ($app) {
     $app->status(404);
