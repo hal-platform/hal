78c5a9c990b4b13abbcfb400c6df8f301be2e039
Matt Colf <MatthewColf@quickenloans.com>
2014-05-13 13:49:52 -0400

    Starting to add servers page.

--- src/QL/Hal/Controllers/Server/ServersController.php
+++ src/QL/Hal/Controllers/Server/ServersController.php
@@ -2,6 +2,12 @@
 
 namespace QL\Hal\Controllers\Server;
 
+use QL\Hal\Core\Entity\Repository\ServerRepository;
+use Twig_Template;
+use Slim\Http\Request;
+use Slim\Http\Response;
+use QL\Hal\Layout;
+
 /**
  *  Servers Controller
  *
@@ -9,5 +15,53 @@ namespace QL\Hal\Controllers\Server;
  */
 class ServersController
 {
+    /**
+     *  @var Twig_Template
+     */
+    private $template;
 
+    /**
+     *  @var Layout
+     */
+    private $layout;
+
+    /**
+     *  @var ServerRepository
+     */
+    private $serverRepo;
+
+    /**
+     *  @param Twig_Template $template
+     *  @param Layout $layout
+     *  @param ServerRepository $serverRepo
+     */
+    public function __construct(
+        Twig_Template $template,
+        Layout $layout,
+        ServerRepository $serverRepo
+    ) {
+        $this->template = $template;
+        $this->layout = $layout;
+        $this->serverRepo = $serverRepo;
+    }
+
+    /**
+     *  @param Request $request
+     *  @param Response $response
+     *  @param array $params
+     *  @param callable $notFound
+     */
+    public function __invoke(Request $request, Response $response, array $params = [], callable $notFound = null)
+    {
+        $servers = $this->serverRepo->findAll();
+
+        $response->body(
+            $this->layout->render(
+                $this->template,
+                [
+                    'servers' => $servers
+                ]
+            )
+        );
+    }
 }
