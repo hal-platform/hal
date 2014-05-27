2ac787eba0d826353650795de2d2fdab5a2f5749
Matt Colf <matthewcolf@quickenloans.com>
2014-05-27 13:24:22 -0400

    Working.

--- src/QL/Hal/Controllers/Repository/Build/BuildStartHandleController.php
+++ src/QL/Hal/Controllers/Repository/Build/BuildStartHandleController.php
@@ -8,8 +8,10 @@ use QL\Hal\Core\Entity\Repository\RepositoryRepository;
 use QL\Hal\Core\Entity\Repository\UserRepository;
 use QL\Hal\Helpers\UrlHelper;
 use QL\Hal\Session;
+use Twig_Template;
 use Slim\Http\Request;
 use Slim\Http\Response;
+use QL\Hal\Layout;
 
 /**
  *  Build Start Handle Controller
@@ -18,13 +20,13 @@ use Slim\Http\Response;
  */
 class BuildStartHandleController
 {
-    const VAL_PULL = '#^pull[0-9]+$#';
+    const VAL_PULL = '#^pull/[0-9]+$#';
 
-    const VAL_COMMIT = '#^commit$#';
+    const VAL_COMMIT = '#^none$#';
 
-    const VAL_TAG = '#^refs/tags/(?!/|.*([/.]\.|//|@\{|\\\\))[^\040\177 ~^:?*\[]+(?<!\.lock|[/.])$#';
+    const VAL_TAG = '#^tag/(?!/|.*([/.]\.|//|@\{|\\\\))[^\040\177 ~^:?*\[]+(?<!\.lock|[/.])$#';
 
-    const VAL_BRANCH = '#^refs/heads/(?!/|.*([/.]\.|//|@\{|\\\\))[^\040\177 ~^:?*\[]+(?<!\.lock|[/.])$#';
+    const VAL_BRANCH = '#^(?!/|.*([/.]\.|//|@\{|\\\\))[^\040\177 ~^:?*\[]+(?<!\.lock|[/.])$#';
 
     const VAL_SHA = '#^[0-9a-f]{40}$#';
 
@@ -79,13 +81,12 @@ class BuildStartHandleController
         $env = $this->envRepo->findOneBy(['key' => $request->post('environment', null)]);
 
         if (!$env) {
-            // @todo render form page with error
+            $this->session->addFlash('You must select an environment to build for.');
+            $response->redirect($this->url->urlFor('build.start', ['repo' => $repo->getKey()]), 303);
+            return;
         }
 
-        if (preg_match(self::VAL_BRANCH, $ref)) {
-            // branch
-
-        } elseif (preg_match(self::VAL_TAG, $ref)) {
+        if (preg_match(self::VAL_TAG, $ref)) {
             // tag
 
         } elseif (preg_match(self::VAL_PULL, $ref)) {
@@ -94,24 +95,27 @@ class BuildStartHandleController
         } elseif (preg_match(self::VAL_COMMIT, $ref)) {
             // commit
 
+        } elseif (preg_match(self::VAL_BRANCH, $ref)) {
+            // branch
+
         } else {
-            // @todo render form page with error
+            $this->session->addFlash('You must select a valid git ref.');
+            $response->redirect($this->url->urlFor('build.start', ['repo' => $repo->getKey()]), 303);
+            return;
         }
 
-
-
-        var_dump($ref, $sha, $env);
-
-        var_dump([
-            'push' => preg_match(self::VAL_PULL, $ref),
-            'tag' => preg_match(self::VAL_TAG, $ref),
-            'branch' => preg_match(self::VAL_BRANCH, $ref),
-            'commit' => preg_match(self::VAL_COMMIT, $ref),
-            'sha' => preg_match(self::VAL_SHA, $sha),
-            'env' => preg_match(self::VAL_ENV, $env)
-        ]);
-
-        die();
+//        var_dump($ref, $sha, $env);
+//
+//        var_dump([
+//            'push' => preg_match(self::VAL_PULL, $ref),
+//            'tag' => preg_match(self::VAL_TAG, $ref),
+//            'branch' => preg_match(self::VAL_BRANCH, $ref),
+//            'commit' => preg_match(self::VAL_COMMIT, $ref),
+//            'sha' => preg_match(self::VAL_SHA, $sha),
+//            'env' => preg_match(self::VAL_ENV, $env)
+//        ]);
+//
+//        die();
 
 
         $response->redirect($this->url->urlFor('repo', ['repo' => $repo->getKey()]), 303);
