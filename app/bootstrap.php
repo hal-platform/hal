0cc23fa681af0052c69a4ec54a6a0b7e5df4cd2b
Steve Kluck <stevekluck@quickenloans.com>
2014-05-28 12:40:33 -0400

    Update all the bootstraps
    - Add routes config to be imported from config.yml
    - Add new hook services
    - Add slim configurator service
    - Simplify bootstrap.php and index.php

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -1,53 +1,52 @@
 <?php
 /**
- * @copyright ©2013 Quicken Loans Inc. All rights reserved. Trade Secret,
+ * @copyright ©2014 Quicken Loans Inc. All rights reserved. Trade Secret,
  *    Confidential and Proprietary. Any dissemination outside of Quicken Loans
  *    is strictly prohibited.
  */
 
-namespace QL\Hal;
+namespace QL\Hal\Deadzone;
 
-use Exception;
+use QL\Hal\CachedContainer;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 
-const CONFIG_FILE       = 'app/config.yml';
-const ROUTES_FILE       = 'app/routes.yml';
-
-$root = __DIR__.'/../';
-require_once $root.'/vendor/autoload.php';
-
-/**
- *  Check if a file exists and is readable the path
- *
- *  @param string $path
- *  @return string
- *  @throws Exception
- */
-function check_file($path)
-{
-    if (is_readable($path)) {
-        return $path;
-    } else {
-        throw new Exception("File $path is not readable");
-    }
-}
-
-$locator    = new FileLocator($root);
-
-// Check Required Files
-check_file($locator->locate(CONFIG_FILE));
-check_file($locator->locate(ROUTES_FILE));
+$root = __DIR__ . '/../';
+require_once $root . '/vendor/autoload.php';
 
 // Set Timezone to UTC
 ini_set('date.timezone', 'UTC');
 date_default_timezone_set('UTC');
 
 // DI Container
-$container = new ContainerBuilder;
-$builder = new YamlFileLoader($container, $locator);
-$builder->load(CONFIG_FILE);
-$container->setParameter('root', $root);
+$isCli = php_sapi_name() === 'cli';
+if (!class_exists('QL\Hal\CachedContainer') || $isCli) {
+    $container = buildDi($root);
+
+} else {
+    $container = new CachedContainer;
+
+    // This is WEIRD, but its the only way to force a refresh container in a specific mode
+    // if ($container->getParameter('debug')) {
+    //     $container = buildDi($root);
+    // }
+}
+
+// runtime config
 $container->set('dic', $container);
-$container->compile();
+
+return $container;
+
+function buildDi($root)
+{
+    $container = new ContainerBuilder;
+    $builder = new YamlFileLoader($container, new FileLocator($root));
+    $builder->load('app/config.yml');
+
+    $container->setParameter('root', $root);
+
+    $container->compile();
+
+    return $container;
+}
--- public/index.php
+++ public/index.php
@@ -5,38 +5,15 @@
  *    is strictly prohibited.
  */
 
-namespace QL\Hal;
+namespace QL\Hal\Deadzone;
 
-use Exception;
-use MCP\DataType\IPv4Address;
-
-require_once __DIR__.'/../app/bootstrap.php';
+$root = __DIR__ . '/../';
+if (!$container = @include $root . '/app/bootstrap.php') {
+    echo "Boom goes the dynamite.\n";
+    return;
+};
 
 // Application
 $app = $container->get('slim');
-$app->view($container->get('twigView'));
-
-// 404 Error Handler
-$app->notFound(function () use ($app) {
-    $app->status(404);
-    $app->render('error.html.twig', array('message' => 'Page Not Found'));
-});
-
-// 500 Error Handler
-$app->error(function (Exception $e) use ($app) {
-    $app->status(500);
-    $app->render('error.html.twig', array('message' => 'Oh, snap! You broke it.'));
-});
-
-// Load Routes
-$routeLoader = new RouteLoader($locator, $app, $container);
-$routeLoader->load(ROUTES_FILE);
-
-// Add Twig Globals
-$twig = $container->get('twigEnv');
-$twig->addGlobal('account', $container->get('session')->get('account'));
-$twig->addGlobal('session', $container->get('session'));
-//$twig->getExtension('core')->setTimezone('America/Detroit');
-
 $app->response()->header('Content-Type', 'text/html; charset=utf-8');
 $app->run();
