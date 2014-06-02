8459c0d58f4e7373d427013d63c44750e7a70525
Steve Kluck <stevekluck@quickenloans.com>
2014-06-02 09:47:29 -0400

    Update /repository urls to /repositories

---
+++ src/QL/Hal/Controllers/Repository/Deployment/DeploymentsController.php
@@ -0,0 +1,86 @@
+<?php
+/**
+ * @copyright ©2014 Quicken Loans Inc. All rights reserved. Trade Secret,
+ *    Confidential and Proprietary. Any dissemination outside of Quicken Loans
+ *    is strictly prohibited.
+ */
+
+namespace QL\Hal\Controllers\Repository\Deployment;
+
+use QL\Hal\Core\Entity\Repository\DeploymentRepository;
+use QL\Hal\Core\Entity\Repository\EnvironmentRepository;
+use QL\Hal\Core\Entity\Repository\RepositoryRepository;
+use QL\Hal\Layout;
+use Slim\Http\Request;
+use Slim\Http\Response;
+use Twig_Template;
+
+class DeploymentsController
+{
+    /**
+     *  @var Twig_Template
+     */
+    private $template;
+
+    /**
+     *  @var Layout
+     */
+    private $layout;
+
+    /**
+     *  @var EnvironmentRepository
+     */
+    private $environmentRepo;
+
+    /**
+     *  @var RepositoryRepository
+     */
+    private $repoRepo;
+
+    /**
+     *  @var DeploymentRepository
+     */
+    private $deploymentRepo;
+
+    /**
+     *  @param Twig_Template $template
+     *  @param Layout $layout
+     *  @param EnvironmentRepository $environmentRepo
+     *  @param RepositoryRepository $repoRepo
+     *  @param DeploymentRepository $deploymentRepo
+     */
+    public function __construct(
+        Twig_Template $template,
+        Layout $layout,
+        EnvironmentRepository $environmentRepo,
+        RepositoryRepository $repoRepo,
+        DeploymentRepository $deploymentRepo
+    ) {
+        $this->template = $template;
+        $this->layout = $layout;
+        $this->environmentRepo = $environmentRepo;
+        $this->repoRepo = $repoRepo;
+        $this->deploymentRepo = $deploymentRepo;
+    }
+
+    /**
+     *  @param Request $request
+     *  @param Response $response
+     *  @param array $params
+     *  @param callable $notFound
+     */
+    public function __invoke(Request $request, Response $response, array $params = [], callable $notFound = null)
+    {
+        if (!$repo = $this->repoRepo->find($params['id'])) {
+            return $notFound();
+        }
+
+        $rendered = $this->layout->render($this->template, [
+            'environments' => $this->environmentRepo->findBy([], ['order' => 'ASC']),
+            'repository' => $repo,
+            'deployments' => $this->deploymentRepo->findBy(['repository' => $repo], ['server' => 'ASC'])
+        ]);
+
+        $response->body($rendered);
+    }
+}
