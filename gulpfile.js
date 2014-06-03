ac5f42a77059b25e8ed07adffff3556cd0fbf2bd
Steve Kluck <stevekluck@quickenloans.com>
2014-06-03 11:26:08 -0400

    use bundle exec when compassing

--- gulpfile.js
+++ gulpfile.js
@@ -14,7 +14,8 @@ gulp.task('styles', function() {
             css: 'public/css',
             sass: 'sass',
             style: isDeploy ? 'compressed' : 'compact',
-            require: ['modular-scale', 'breakpoint']
+            require: ['modular-scale', 'breakpoint'],
+            bundle_exec: true
         }))
         .pipe(plugins.autoprefixer('last 2 versions', '> 1%'))
         .pipe(gulp.dest('public/css'))
