d96ceeedfffca7fb57318e94bebc8cbb4b04197a
Steve Kluck <stevekluck@quickenloans.com>
2014-06-03 13:01:17 -0400

    Update terminal markup and add border

--- js/modules/terminal.js
+++ js/modules/terminal.js
@@ -1,7 +1,8 @@
 define(['jquery'], function($) {
     return {
-        target: '.terminal pre',
-        scale: 40,
+        target: '.terminal__entry',
+        scale: 30,
+        longDelay: 10,
         init: function() {
             var container = $(this.target);
 
@@ -9,8 +10,6 @@ define(['jquery'], function($) {
             // visibility flashes while the content is wrapped in spans
             var color = container.css('color');
             container.css('color', 'transparent');
-
-            container.append('<span class="cursor typed"></span>');
 
             // Add empty space so vertical height of terminal is maintained when cursor hits the bottom
             container.append(" ");
@@ -26,66 +25,38 @@ define(['jquery'], function($) {
                     return;
                 }
 
-                $(this).replaceWith($.map(this.nodeValue.split(''), function(character) {
+                $(this).replaceWith($.map(this.nodeValue.trim().concat("\n").split(''), function(character) {
                    return '<span>' + character + '</span>';
                 }).join(''));
             });
         },
         typeCharacters: function($container) {
-            var $characters = $container.children('span');
-            var delay = 0;
-            var fullText = '';
-            var longDelay = 7;
+            var longDelay = this.longDelay;
             var scale = this.scale;
-            var typoDelay = 10;
+            var $characters = $container.children('span');
+            var $cursor = $('<span class="cursor typed"></span>');
+
+            var delay = 0;
+            var lastDelay = 0;
 
             $characters.each(function(index, el) {
                 var $char = $(el);
                 var char = $char.text();
-                var isALetter = /[A-Za-z]/.test(char);
-                var makeATypo = _getRandomNumberBetween(1, fullText.length) === 30 && isALetter;
-
-                fullText += char;
-                delay += !isALetter && char !== ' ' ? longDelay : _getRandomNumberBetween(1, 3);
+                var isStandardCharacter = /[A-Za-z\,\ \']/.test(char);
+                lastDelay = delay * scale;
 
                 setTimeout(function() {
-                    var $oopsChar;
-                    var oopsChar;
+                    $char.addClass('typed').after($cursor);
+                }, lastDelay);
 
-                    if (makeATypo) {
-                        $oopsChar = $($characters.get(index + _getRandomNumberBetween(1, 2)));
-                        oopsChar = $oopsChar.text();
-
-                        if (char === char.toUpperCase()) {
-                          oopsChar = oopsChar.toUpperCase();
-                        } else {
-                          oopsChar = oopsChar.toLowerCase();
-                        }
-
-                        $char.text(oopsChar);
-                        $char.addClass('typed');
-
-                        setTimeout(function() {
-                            $char.removeClass('typed');
-
-                            setTimeout(function() {
-                                $char.text(char);
-                                $char.addClass('typed');
-                            }, typoDelay * scale / 2);
-                        }, typoDelay * scale / 2);
-                    } else {
-                        $char.addClass('typed');
-                    }
-                }, delay * scale);
-
-                if (makeATypo) {
-                    delay += typoDelay;
-                }
+                // Set the delay for the next character.
+                delay += isStandardCharacter ? 1 : longDelay;
             });
 
-            function _getRandomNumberBetween(min, max) {
-                return Math.ceil(Math.random(min, max) * max);
-            }
+            // remove the empty space inserted, this leaves only the cursor on the last line
+            setTimeout(function() {
+                $container.children('span:nth-last-child(2)').remove();
+            }, lastDelay);
         }
     };
 });
