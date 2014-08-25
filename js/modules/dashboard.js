55cd2dacd175108a1413d5b31535482826d5257b
Jen <jenmay@quickenloans.com>
2014-08-25 13:33:09 -0400

    fixed tab issue

--- js/modules/dashboard.js
+++ js/modules/dashboard.js
@@ -1,44 +1,44 @@
 define(['jquery'], function($) {
-		return {
-				filterList: '.js-filter',
-				inputTarget: '.js-filter__input',
-				toggleBtn: '.js-hide-btn',
-				hideContent: '.js-hide-box',
-				init: function() {
-					this.toggle();
+    return {
+        filterList: '.js-filter',
+        inputTarget: '.js-filter__input',
+        toggleBtn: '.js-hide-btn',
+        hideContent: '.js-hide-box',
+        init: function() {
+            this.toggle();
 
-					if (this.filterList.length !==0){
-							this.filterRepos();
-					}
-				},
-				filterRepos: function(){
-						var repoList = $(this.filterList + '> li');
-						var searchField = $(this.inputTarget);
-						var _this = this;
+            if (this.filterList.length !==0){
+                this.filterRepos();
+            }
+        },
+        filterRepos: function(){
+            var repoList = $(this.filterList + '> li');
+            var searchField = $(this.inputTarget);
+            var _this = this;
 
-						searchField.keyup(function(){
-								var searchVal = searchField.val().toLowerCase();
-								_this.delay(function(){
-										repoList.each(function(){
-												var txt = $(this).text().toLowerCase();
+            searchField.keyup(function(){
+                var searchVal = searchField.val().toLowerCase();
+                _this.delay(function(){
+                    repoList.each(function(){
+                        var txt = $(this).text().toLowerCase();
 
-												$(this).toggle(txt.indexOf(searchVal) === 0);
-										});
-								}, 500);
-						});
-				},
-				toggle: function(){
-						var btn = $(this.toggleBtn);
-						var box = $(this.hideContent);
+                        $(this).toggle(txt.indexOf(searchVal) === 0);
+                    });
+                }, 500);
+            });
+        },
+        toggle: function(){
+            var btn = $(this.toggleBtn);
+            var box = $(this.hideContent);
 
-						btn.click(function(){
-								box.slideToggle("slow");
-						});
-				},
-				delay: function(callback, ms) {
-						var timer = 0;
-						clearTimeout(timer);
-						timer = setTimeout(callback, ms);
-				},
-		};
+            btn.click(function(){
+                box.slideToggle("slow");
+            });
+        },
+        delay: function(callback, ms) {
+            var timer = 0;
+            clearTimeout(timer);
+            timer = setTimeout(callback, ms);
+        }
+    };
 });
