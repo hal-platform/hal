2f69a0b7455aa2b15a65e02e2501e5fe3718bb41
Bridget Schiefer <BridgetSchiefer@QuickenLoans.com>
2014-09-26 13:40:11 -0400

    Added logging to core

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -45,3 +45,4 @@ function buildDi($root)
 
     return $container;
 }
+
--- src/QL/Hal/Slim/ErrorPagesHook.php
+++ src/QL/Hal/Slim/ErrorPagesHook.php
@@ -11,6 +11,8 @@ use Exception;
 use Slim\Slim;
 use Twig_Template;
 use PDOException;
+use Psr\Log\LoggerInterface;
+use Slim\Http\Response;
 
 /**
  * Define error page handlers.
@@ -20,15 +22,29 @@ use PDOException;
 class ErrorPagesHook
 {
     /**
+     * @var LoggerInterface
+     */
+    private $logger;
+
+    /**
+     * @var boolean
+     */
+    private $isDebugMode;
+
+    /**
      * @var Twig_Template
      */
     private $twig;
 
     /**
+     * @param LoggerInterface $logger
+     * @param boolean $isDebugMode
      * @param Twig_Template $twig
      */
-    public function __construct(Twig_Template $twig)
+    public function __construct(LoggerInterface $logger, $isDebugMode, Twig_Template $twig)
     {
+        $this->logger = $logger;
+        $this->isDebugMode = $isDebugMode;
         $this->twig = $twig;
     }
 
@@ -50,16 +66,19 @@ class ErrorPagesHook
         // 500 Error Handler
         $app->error(function (Exception $e) use ($app) {
             $message = $e->getMessage();
+            $context = ['exceptionData' => $e->getTraceAsString()];
 
             if ($e instanceof PDOException) {
                 $message = "There's a problem with the database. Wait a bit and try again.\r\n".$message;
             }
+
+            $this->logger->error($message, $context);
 
             $output = $this->twig->render(['message' => $message]);
 
             $app->status(500);
             $app->response()->write($output);
             $app->stop();
-        });
+        }); 
     }
 }
