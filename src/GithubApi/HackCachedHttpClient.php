933bd7203c96ee227b227a7823a42d989623bbc3
Steve Kluck <stevekluck@quickenloans.com>
2014-10-02 12:14:21 -0400

    Fix the shitty github pager

--- src/GithubApi/HackCachedHttpClient.php
+++ src/GithubApi/HackCachedHttpClient.php
@@ -10,6 +10,7 @@ namespace QL\Hal\GithubApi;
 use Github\HttpClient\Cache\CacheInterface;
 use Github\HttpClient\Cache\FilesystemCache;
 use Github\HttpClient\HttpClient;
+use Guzzle\Http\Message\Response;
 
 /**
  * Extending the knplabs api to not be completely horrible and stupid.
@@ -22,6 +23,13 @@ class HackCachedHttpClient extends HttpClient
      * @var CacheInterface
      */
     protected $cache;
+
+    /**
+     * Contains the lastResponse fetched from cache
+     *
+     * @var Response
+     */
+    private $lastCachedResponse;
 
     /**
      * @return CacheInterface
@@ -48,14 +56,13 @@ class HackCachedHttpClient extends HttpClient
      */
     public function request($path, $body = null, $httpMethod = 'GET', array $headers = array(), array $options = array())
     {
-        $cacheKey = $path;
-        if (isset($options['query'])) {
-            $cacheKey .= serialize($options['query']);
-        }
+        $cacheKey = $this->buildCacheKey($path, $options);
+
         $response = parent::request($path, $body, $httpMethod, $headers, $options);
 
         if (304 == $response->getStatusCode()) {
-            return $this->getCache()->get($cacheKey);
+            $this->lastCachedResponse = $this->getCache()->get($cacheKey);
+            return $this->lastCachedResponse;
         }
 
         $this->getCache()->set($cacheKey, $response);
@@ -69,11 +76,9 @@ class HackCachedHttpClient extends HttpClient
      */
     protected function createRequest($httpMethod, $path, $body = null, array $headers = array(), array $options = array())
     {
-        $cacheKey = $path;
-        if (isset($options['query'])) {
-            $cacheKey .= serialize($options['query']);
-        }
-        $request = parent::createRequest($httpMethod, $path, $body, $headers = array(), $options);
+        $cacheKey = $this->buildCacheKey($path, $options);
+
+        $request = parent::createRequest($httpMethod, $path, $body, $headers, $options);
 
         if ($etag = $this->getCache()->getETag($cacheKey)) {
             $request->addHeader(
@@ -83,5 +88,36 @@ class HackCachedHttpClient extends HttpClient
         }
 
         return $request;
+    }
+
+    /**
+     * @return Response
+     */
+    public function getLastResponse($force = false)
+    {
+        $lastResponse =  parent::getLastResponse();
+        if (304 != $lastResponse->getStatusCode()) {
+            $force = true;
+        }
+
+        return ($force) ? $lastResponse : $this->lastCachedResponse;
+    }
+
+    /**
+     * Append the query string to the path for generating a unique cache key
+     *
+     * @param string $path
+     * @param array $options
+     *
+     * @return string
+     */
+    private function buildCacheKey($path, $options)
+    {
+        $cacheKey = $path;
+        if (isset($options['query'])) {
+            $cacheKey .= serialize($options['query']);
+        }
+
+        return $cacheKey;
     }
 }
