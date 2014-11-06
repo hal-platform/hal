04d77f4bb16906b991accd4363bb28c1fea4042f
Steve Kluck <stevekluck@quickenloans.com>
2014-11-06 12:54:13 -0500

    Load env specific requirejs config from a flat json file

--- js/main.js
+++ js/main.js
@@ -1,23 +1,27 @@
-define(['controller/_common'], function(app) {
-    var date = new Date();
+require.config({
+    // Make sure "shim" and "paths" match the same values in optimizer.json!
+    "shim": {
+        "underscore": {
+            "exports": "_"
+        }
+    },
+    "paths": {
+        "crossroads": "vendor/crossroads.min",
+        "jquery": "vendor/jquery-2.min",
+        "jquerySortable": "vendor/jquery.sortable.min",
+        "jqueryTablesaw": "vendor/jquery.tablesaw",
+        "nunjucks": "vendor/nunjucks.min",
+        "signals": "vendor/signals.min",
+        "tablesaw": "vendor/tablesaw.min",
+        "underscore": "vendor/underscore.min",
+        "json": "vendor/requirejs-plugins/json",
+        "text": "vendor/requirejs-plugins/text"
+    }
+});
 
-    require.config({
-        shim: {
-            underscore: {
-                exports: '_'
-            }
-          },
-        paths: {
-            crossroads: 'vendor/crossroads.min',
-            jquery: 'vendor/jquery-2.min',
-            jquerySortable: 'vendor/jquery.sortable.min',
-            nunjucks: 'vendor/nunjucks.min',
-            signals: 'vendor/signals.min',
-            tablesaw: 'vendor/tablesaw.min',
-            underscore: 'vendor/underscore.min'
-        },
-        urlArgs: "" + date.getFullYear() + date.getMonth() + date.getDate()
-    });
+require(['controller/_common', 'json!config.json'], function(app, config) {
 
+    // further requirejs config
+    require.config(config);
     app.init();
 });
