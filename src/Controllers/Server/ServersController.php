8b89a1e0cab31b2ce137d8717ff53aedbd65a4b8
Steve Kluck <stevekluck@quickenloans.com>
2014-11-14 13:44:32 -0500

    Make the server page usable

--- src/Controllers/Server/ServersController.php
+++ src/Controllers/Server/ServersController.php
@@ -8,6 +8,7 @@
 namespace QL\Hal\Controllers\Server;
 
 use QL\Hal\Core\Entity\Repository\ServerRepository;
+use QL\Hal\Core\Entity\Server;
 use Slim\Http\Request;
 use Slim\Http\Response;
 use Twig_Template;
@@ -42,10 +43,63 @@ class ServersController
      */
     public function __invoke(Request $request, Response $response, array $params = [], callable $notFound = null)
     {
+        $servers = $this->serverRepo->findBy([], ['name' => 'ASC']);
+
         $rendered = $this->template->render([
-            'servers' => $this->serverRepo->findBy([], ['environment' => 'ASC', 'name' => 'ASC'])
+            'server_environments' => $this->sortStandard($servers),
+            'unknown_server_environments' => $this->sortUnknown($servers),
+            'server_count' => count($servers)
         ]);
 
         $response->setBody($rendered);
+    }
+
+    /**
+     * @param Server[] $servers
+     * @return array
+     */
+    private function sortStandard(array $servers)
+    {
+        $environments = [
+            'dev' => [],
+            'test' => [],
+            'beta' => [],
+            'prod' => []
+        ];
+
+        foreach ($servers as $server) {
+            $env = $server->getEnvironment()->getKey();
+
+            if (in_array($env, ['dev', 'test', 'beta', 'prod'])) {
+                $environments[$env][] = $server;
+            }
+        }
+
+        return $environments;
+    }
+
+    /**
+     * @param Server[] $servers
+     * @return array
+     */
+    private function sortUnknown(array $servers)
+    {
+        $environments = [];
+
+        foreach ($servers as $server) {
+            $env = $server->getEnvironment()->getKey();
+
+            if (in_array($env, ['dev', 'test', 'beta', 'prod'])) {
+                continue;
+            }
+
+            if (!array_key_exists($env, $environments)) {
+                $environments[$env] = [];
+            }
+
+            $environments[$env][] = $server;
+        }
+
+        return $environments;
     }
 }
