e3f55959b8f9b056cf2f10305a526ddb3f9ccbaf
Steve Kluck <stevekluck@quickenloans.com>
2014-11-17 18:45:42 -0500

    First rev of rebuilt error page

--- js/modules/nofunzone.js
+++ js/modules/nofunzone.js
@@ -1,128 +1,39 @@
-define(['jquery'], function($) {
+define(['jquery', 'vendor/typed'], function($, typed) {
     return {
         target: '[data-nofunzone]',
+        initialAttr: 'initial',
         givenAttr: 'nofunzone',
         freudAttr: 'freud',
-        $target: null,
-
-        fullFreud: null,
-        fullGiven: null,
-        delay: 2500,
 
         init: function() {
 
-            this.$target = $(this.target);
-            var given = this.$target.data(this.givenAttr),
-                freud = this.$target.data(this.freudAttr);
+            var $target = $(this.target);
+            var initial = $target.data(this.initialAttr),
+                freud = $target.data(this.freudAttr),
+                given = $target.data(this.givenAttr);
 
-            if (given && freud && given.length && freud.length) {
-                // Add cursor
-                this.$target.addClass('univCursor');
+            if (given && given.length) {
+                var typedStrings = [initial];
 
-                // Type
-                var steps = this.buildSteps(this.$target.text(), freud, given);
-                this.runTyping(steps);
-
-                // Add more delay to ensure these actions are at the very end of all steps
-                this.delay += 500;
-
-                // Remove cursor
-                var trg = this.$target;
-                setTimeout(function() {
-                    trg.removeClass('univCursor');
-                }, this.delayer());
-
-                // If the tab is inactive, the order of the events may get messed up.
-                // So just in case, at the very end, set to the correct name again.
-                var fgiven = this.fullGiven;
-                var setName = function() {
-                    trg.text(fgiven);
-                };
-                setTimeout(setName, this.delayer());
-            }
-        },
-        runTyping: function(steps) {
-            var cb = null,
-                useSlowdown = false;
-
-            // create steps
-            for (i = 0; i < steps.length; i++) {
-                if (i > 0) {
-                    useSlowdown = (steps[i].length > steps[i-1].length);
+                if (freud && freud.length) {
+                    typedStrings.push(freud);
+                } else {
+                    $target.text('');
                 }
 
-                cb = this.setText(this.$target, steps[i]);
-                setTimeout(cb, this.stepDelayer(i, useSlowdown));
+                typedStrings.push(given);
 
-                // let that sink for a second
-                if (steps[i] == this.fullFreud) {
-                    this.delay += 2000;
-                }
+                $target.typed({
+                    strings: typedStrings,
+                    startDelay: 1000,
+                    typeSpeed: 50,
+                    backSpeed: 25,
+                    backDelay: 1000,
+                    callback: function() {
+                        $target.parent().children('.typed-cursor').remove();
+                    }
+                });
             }
-
         },
-        buildSteps: function(current, freud, given) {
-
-            var initialLength = current.length,
-                steps = [],
-                fullFreud = null;
-
-            // If the target text has a prefix of "Hello, ", adjust the length so we do not remove it.
-            var match = /^[\s]*Hello, ([\x21-\x7E\s]+)$/i.exec(current);
-            if (match !== null && match.length > 0) {
-                initialLength = match[1].length;
-            }
-
-            // Remove initial
-            for (i = 0; i < initialLength; i++) {
-                current = current.slice(0, -1);
-                steps.push(current);
-            }
-
-            // Add freudian slip
-            for (i = 0; i < freud.length; i++) {
-                current = current + freud.charAt(i);
-                steps.push(current);
-            }
-
-            // record fully built freud name
-            this.fullFreud = current;
-
-            // Remove freud
-            for (i = 0; i < freud.length; i++) {
-                current = current.slice(0, -1);
-                steps.push(current);
-            }
-
-            // Add given
-            for (i = 0; i < given.length; i++) {
-                current = current + given.charAt(i);
-                steps.push(current);
-            }
-
-            // record fully built given name
-            this.fullGiven = current;
-
-            return steps;
-        },
-        setText: function($target, text) {
-            return function () {
-                $target.text(text);
-            };
-        },
-        stepDelayer: function (step, addSlowDown) {
-            // if 4th delete/add, add extra delay time
-            if (addSlowDown && step % 4 === 0) {
-                this.delay += 400;
-            }
-            this.delay += 100;
-
-            return this.delay;
-        },
-        delayer: function () {
-            this.delay += 100;
-
-            return this.delay;
-        }
     };
 });
--- js/modules/terminal.js
+++ js/modules/terminal.js
@@ -1,62 +1,19 @@
-define(['jquery'], function($) {
+define(['jquery', 'vendor/typed'], function($, typed) {
     return {
         target: '.terminal__entry',
-        scale: 30,
-        longDelay: 10,
         init: function() {
-            var container = $(this.target);
 
-            // pop the color immediately. This is done asap so the user doesn't get any weird
-            // visibility flashes while the content is wrapped in spans
-            var color = container.css('color');
-            container.css('color', 'transparent');
+            var $target = $(this.target);
+            var $text = $('.terminal__text');
 
-            // Add empty space so vertical height of terminal is maintained when cursor hits the bottom
-            container.append(" ");
+            if ($target.length && $text.length) {
+                var text = $text.text().trim();
 
-            this.wrapCharacters(container);
-            container.css('color', color);
-
-            this.typeCharacters(container);
-        },
-        wrapCharacters: function($container) {
-            $container.contents().each(function() {
-                if (this.nodeType !== 3) {
-                    return;
-                }
-
-                $(this).replaceWith($.map(this.nodeValue.trim().concat("\n").split(''), function(character) {
-                   return '<span>' + character + '</span>';
-                }).join(''));
-            });
-        },
-        typeCharacters: function($container) {
-            var longDelay = this.longDelay;
-            var scale = this.scale;
-            var $characters = $container.children('span');
-            var $cursor = $('<span class="cursor typed"></span>');
-
-            var delay = 0;
-            var lastDelay = 0;
-
-            $characters.each(function(index, el) {
-                var $char = $(el);
-                var char = $char.text();
-                var isDelayedCharacter = /\r\n|\n|\r/.test(char);
-                lastDelay = delay * scale;
-
-                setTimeout(function() {
-                    $char.addClass('typed').after($cursor);
-                }, lastDelay);
-
-                // Set the delay for the next character.
-                delay += isDelayedCharacter ? longDelay : 1;
-            });
-
-            // remove the empty space inserted, this leaves only the cursor on the last line
-            setTimeout(function() {
-                $container.children('span:nth-last-child(2)').remove();
-            }, lastDelay);
+                $target.typed({
+                    strings: [text],
+                    showCursor: false
+                });
+            }
         }
     };
 });
