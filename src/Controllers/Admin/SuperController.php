9e8194c49e7c19012b715cc028cbbdb9675ad489
Steve Kluck <stevekluck@quickenloans.com>
2014-11-21 14:39:08 -0500

    Clean up super admin doctrine clearer and add hal push file dumper

--- src/Controllers/Admin/SuperController.php
+++ src/Controllers/Admin/SuperController.php
@@ -25,13 +25,20 @@ class SuperController
     private $doctrineConfig;
 
     /**
+     * @type string
+     */
+    private $halPushFile;
+
+    /**
      * @param Twig_Template $template
      * @param Configuration $doctrineConfig
+     * @param string $halPushFile
      */
-    public function __construct(Twig_Template $template, Configuration $doctrineConfig)
+    public function __construct(Twig_Template $template, Configuration $doctrineConfig, $halPushFile)
     {
         $this->template = $template;
         $this->doctrineConfig = $doctrineConfig;
+        $this->halPushFile = $halPushFile;
     }
 
     /**
@@ -40,45 +47,46 @@ class SuperController
      */
     public function __invoke(Request $request, Response $response)
     {
-        $body = 'No action';
+        $context = [
+            'servername' => gethostname()
+        ];
 
-        if ($request->get('clear_doctrine_cache')) {
-            $cacheStatus = [];
-
-            $cacheStatus = array_merge($cacheStatus, $this->clearCache('Query', 'getQueryCacheImpl'));
-            $cacheStatus = array_merge($cacheStatus, $this->clearCache('Hydration', 'getHydrationCacheImpl'));
-            $cacheStatus = array_merge($cacheStatus, $this->clearCache('Metadata', 'getMetadataCacheImpl'));
-
-            $cacheStatus[] = 'Protip: Smash the reload button to make sure the cache is cleared on ALL SERVERS!';
-            $body = implode("\n\n====================\n\n", $cacheStatus);
+        # add hal push file if possible.
+        if (file_exists($this->halPushFile)) {
+            $context['pushfile'] = file_get_contents($this->halPushFile);
         }
 
-        $rendered = $this->template->render([
-            'old_content' => $body,
-            'servername' => gethostname()
-        ]);
+        # clear doctrine
+        if ($request->get('clear_doctrine')) {
+            $context['doctrine_status'] = [
+                'Query' => $this->clearDoctrine('getQueryCacheImpl'),
+                'Hydration' => $this->clearDoctrine('getHydrationCacheImpl'),
+                'Metadata' => $this->clearDoctrine('getMetadataCacheImpl')
+            ];
+        }
+
+        # clear permissions
+        if ($request->get('clear_permissions')) {
+
+        }
+
+        $rendered = $this->template->render($context);
 
         $response->setBody($rendered);
     }
 
     /**
-     * @param string $type
      * @param string $accessor
-     * @return array
+     *
+     * @return string
      */
-    private function clearCache($type, $accessor)
+    private function clearDoctrine($accessor)
     {
         if (!$cache = $this->doctrineConfig->$accessor()) {
-            $body = sprintf("%s Cache Type:\nMissing", $type);
-            return [$body];
+            return 'Cache missing.';
         }
 
-        $body = sprintf("%s Cache Type:\n%s\n\n", $type, get_class($cache));
-
-        $body .= sprintf(" > %s Cache reset!\n", $type);
-
         $cache->deleteAll();
-
-        return [$body];
+        return sprintf('"%s" reset.', get_class($cache));
     }
 }
