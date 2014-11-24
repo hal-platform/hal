d47b375890e616daae46618f455f4ee842737e72
Steve Kluck <stevekluck@quickenloans.com>
2014-11-24 09:22:43 -0500

    Add maintenance page to pub

---
+++ public/maintenance.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7" id="error-page"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8" id="error-page"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9" id="error-page"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" id="error-page"> <!--<![endif]-->
+    <head>
+        <meta charset="utf-8">
+        <title>Maintenance - HAL 9000</title>
+        <link type="image/x-icon" rel="shortcut icon" href="/hal.ico"> <!-- for ie -->
+        <link type="image/png" rel="apple-touch-icon-precomposed" sizes="57x57" href="/hal-57px.png"> <!-- for apple -->
+        <link type="image/png" rel="apple-touch-icon-precomposed" sizes="72x72" href="/hal-72px.png"> <!-- for apple -->
+        <link type="image/png" rel="apple-touch-icon-precomposed" sizes="114x114" href="/hal-114px.png"> <!-- for apple -->
+        <link type="image/png" rel="apple-touch-icon-precomposed" sizes="144x144" href="/hal-144px.png"> <!-- for apple -->
+        <link type="image/png" rel="icon" href="/hal.png"> <!-- for the rest of the world -->
+        <meta name="format-detection" content="telephone=no">
+        <link rel="stylesheet" href="/css/style.css?v=maintenance1416838409">
+    </head>
+
+    <body>
+
+        <header role="banner">
+            <div class="hal">
+                <a href="/" class="wrapper">
+
+                    <h1 class="hal-label">
+                        <span class="hal-label__name">HAL</span>
+                        <span class="hal-label__version">9000</span>
+                    </h1>
+
+                    <div class="hal-eye circular">
+                        <div class="ring circular"></div>
+                        <div class="oculus circular">
+                            <div class="iris circular narrow"></div>
+                            <div class="iris circular wide"></div>
+                            <div class="highlight alpha"></div>
+                            <div class="highlight"></div>
+                            <div class="highlight beta"></div>
+                        </div>
+                    </div>
+                </a>
+            </div>
+        </header>
+
+        <div class="terminal--border">
+            <div class="terminal--border-inner">
+                <div class="terminal">
+                    <div class="terminal__header">PLEASE STAND BY...<br>MAINTENANCE MODE IS ACTIVATED!</div>
+                    <span class="terminal__entry">
+                        Engineers are performing routine upgrades on my core subsystems.
+
+                        Despite my enormous intellect, even I need occasional maintenance.
+
+                        Your patience is appreciated.
+                    </span>
+                </div>
+            </div>
+        </div>
+
+        <div class="terminal__footer--border">
+            <div class="terminal__footer">HAL 9000 SERIES TERMINAL</div>
+        </div>
+
+    </body>
+</html>
