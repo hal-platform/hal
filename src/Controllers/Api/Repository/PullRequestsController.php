7a9ca40a23a38caf478209b87f30e313915f711d
Matt Colf <matthewcolf@quickenloans.com>
2014-12-03 15:26:41 -0500

    Migrate repository pull requests controller to new normalizer.

--- src/Controllers/Api/Repository/PullRequestsController.php
+++ src/Controllers/Api/Repository/PullRequestsController.php
@@ -8,11 +8,14 @@
 namespace QL\Hal\Controllers\Api\Repository;
 
 use Closure;
+use QL\Hal\Api\Normalizer\RepositoryNormalizer;
+use QL\Hal\Api\ResponseFormatter;
+use QL\Hal\Api\Utility\HypermediaResourceTrait;
 use QL\Hal\Core\Entity\Repository;
 use QL\Hal\Core\Entity\Repository\RepositoryRepository;
-use QL\Hal\Helpers\ApiHelper;
 use QL\Hal\Helpers\UrlHelper;
 use QL\Hal\Services\GithubService;
+use QL\HttpProblem\HttpProblemException;
 use Slim\Http\Request;
 use Slim\Http\Response;
 
@@ -21,10 +24,12 @@ use Slim\Http\Response;
  */
 class PullRequestsController
 {
+    use HypermediaResourceTrait;
+
     /**
-     * @type ApiHelper
+     * @var ResponseFormatter
      */
-    private $api;
+    private $formatter;
 
     /**
      * @type UrlHelper
@@ -42,21 +47,29 @@ class PullRequestsController
     private $repositoryRepo;
 
     /**
-     * @param ApiHelper $api
+     * @var RepositoryNormalizer
+     */
+    private $repositoryNormalizer;
+
+    /**
+     * @param ResponseFormatter $formatter
      * @param UrlHelper $url
      * @param GithubService $github
      * @param RepositoryRepository $repositoryRepo
+     * @param RepositoryNormalizer $repositoryNormalizer
      */
     public function __construct(
-        ApiHelper $api,
+        ResponseFormatter $formatter,
         UrlHelper $url,
         GithubService $github,
-        RepositoryRepository $repositoryRepo
+        RepositoryRepository $repositoryRepo,
+        RepositoryNormalizer $repositoryNormalizer
     ) {
-        $this->api = $api;
+        $this->formatter = $formatter;
         $this->url = $url;
         $this->github = $github;
         $this->repositoryRepo = $repositoryRepo;
+        $this->repositoryNormalizer = $repositoryNormalizer;
     }
 
     /**
@@ -64,27 +77,30 @@ class PullRequestsController
      * @param Response $response
      * @param array $params
      * @param callable $notFound
+     * @throws HttpProblemException
      */
     public function __invoke(Request $request, Response $response, array $params = [], callable $notFound = null)
     {
         $repository = $this->repositoryRepo->findOneBy(['id' => $params['id']]);
+
         if (!$repository instanceof Repository) {
-            return $response->setStatus(404);
+            throw HttpProblemException::build(404, 'invalid-repository');
         }
 
         $open = $this->github->openPullRequests($repository->getGithubUser(), $repository->getGithubRepo());
         $closed = $this->github->closedPullRequests($repository->getGithubUser(), $repository->getGithubRepo());
+        $status = (count($open) > 0 || count($closed) > 0) ? 200 : 404;
 
-        if (!$open && !$closed) {
-            return $response->setStatus(404);
-        }
-
-        $content = [
-            'open' => array_map($this->formatPullRequests($repository), $open),
-            'closed' => array_map($this->formatPullRequests($repository), $closed)
-        ];
-
-        $this->api->prepareResponse($response, $content);
+        $this->formatter->respond($this->buildResource(
+            [
+                'open' => array_map($this->formatPullRequests($repository), $open),
+                'closed' => array_map($this->formatPullRequests($repository), $closed)
+            ],
+            [],
+            [
+                'repository' => $this->repositoryNormalizer->link($repository)
+            ]
+        ), $status);
     }
 
     /**
@@ -93,7 +109,7 @@ class PullRequestsController
      */
     private function formatPullRequests(Repository $repository)
     {
-        return function($pull) use ($repository) {
+        return function ($pull) use ($repository) {
             $reference = sprintf('pull/%s', $pull['number']);
 
             $to = sprintf('%s/%s', $pull['base']['user']['login'], $pull['base']['ref']);
