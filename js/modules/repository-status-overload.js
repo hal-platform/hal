b775d3d9307ce3ba1173f4ccb668785e3254976a
Steve Kluck <stevekluck@quickenloans.com>
2015-01-16 16:05:18 -0500

    Make push expander a toggle

--- js/modules/repository-status-overload.js
+++ js/modules/repository-status-overload.js
@@ -1,29 +1,52 @@
 define(['jquery'], function($) {
     return {
         parent: '.js-overloader-parent',
-        overload: '.js-data-overload',
+        overloaded: '.js-data-overload',
+
+        $overloaded: null,
+        $overloader: null,
+        status: false,
+
+        expandText: 'Expand deployment details',
+        collapseText: 'Collapse deployment details',
 
         init: function() {
 
             var $parent = $(this.parent);
             if ($parent.length > 0) {
+                this.$overloaded = $(this.overloaded);
                 this.attachOverloader($parent);
             }
         },
         attachOverloader: function($parent) {
             var _this = this;
 
-            var $overloader = $('<a href="#">Expand deployment details</a>');
-            $overloader.click(function(event) {
-                event.preventDefault();
-                $(this).remove();
-                $(_this.overload).show();
-            });
+            this.$overloader = $('<a href="#">' + this.expandText + '</a>');
 
             var $wrapper = $('<p class="header--sub"></p>');
-            $wrapper.append($overloader);
-
+            $wrapper.append(this.$overloader);
             $parent.append($wrapper);
+
+            this.$overloader.on('click', function(event) {
+                _this.overloadToggler();
+                event.preventDefault();
+            });
         },
+        overloadToggler: function() {
+
+            var text = this.collapseText;
+            if (this.status) {
+                text = this.expandText;
+            }
+
+            this.$overloader.text(text);
+            if (this.status) {
+                this.$overloaded.hide();
+            } else {
+                this.$overloaded.show();
+            }
+
+            this.status = !this.status;
+        }
     };
 });
