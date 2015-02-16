c2de61415aec7c73ce0a76c4eb83105a7391807b
Steve Kluck <stevekluck@quickenloans.com>
2015-02-16 17:04:02 -0500

    Add org handler for github api

---
+++ src/Github/Organization.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * @copyright ©2014 Quicken Loans Inc. All rights reserved. Trade Secret,
+ *    Confidential and Proprietary. Any dissemination outside of Quicken Loans
+ *    is strictly prohibited.
+ */
+
+namespace QL\Hal\Github;
+
+use Github\Api\Organization as OrganizationApi;
+
+class Organization extends OrganizationApi
+{
+    /**
+     * Request all organizations:
+     *
+     * @link https://developer.github.com/v3/search/#search-users
+     *
+     * @return array list of organizations found
+     */
+    public function all()
+    {
+        return $this->get('search/users', [
+            'q' => 'type:organization'
+        ]);
+    }
+}
