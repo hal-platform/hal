d68da589a73d69a2eb6c4d0e8983d216c371ced6
Steve Kluck <stevekluck@quickenloans.com>
2015-03-13 09:29:05 -0400

    Update audit logger to use jsonable entities

--- src/Doctrine/EntityChangeLogger.php
+++ src/Doctrine/EntityChangeLogger.php
@@ -1,24 +1,19 @@
 <?php
 /**
- * @copyright ©2014 Quicken Loans Inc. All rights reserved. Trade Secret,
+ * @copyright ©2015 Quicken Loans Inc. All rights reserved. Trade Secret,
  *    Confidential and Proprietary. Any dissemination outside of Quicken Loans
  *    is strictly prohibited.
  */
 
 namespace QL\Hal\Doctrine;
 
-use DateTime;
-use DateTimeZone;
 use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\Event\OnFlushEventArgs;
 use Doctrine\ORM\UnitOfWork;
-use MCP\DataType\Time\TimePoint;
+use MCP\DataType\Time\Clock;
 use QL\Hal\Core\Entity\AuditLog;
-use QL\Hal\Core\Entity\Session;
 use QL\Hal\Core\Entity\User;
 use QL\Hal\Helpers\LazyUserHelper;
-use ReflectionClass;
-use Zend\Ldap\Ldap;
 
 class EntityChangeLogger
 {
@@ -27,21 +22,23 @@ class EntityChangeLogger
     const ACTION_DELETE = 'DELETE';
 
     /**
-     * @var EntityManager
-     */
-    private $em;
-
-    /**
-     * @var LazyUserHelper
+     * @type LazyUserHelper
      */
     private $userHelper;
 
     /**
-     * @param LazyUserHelper $userHelper
+     * @type Clock
      */
-    public function __construct(LazyUserHelper $userHelper)
+    private $clock;
+
+    /**
+     * @param LazyUserHelper $userHelper
+     * @param Clock $clock
+     */
+    public function __construct(LazyUserHelper $userHelper, Clock $clock)
     {
         $this->userHelper = $userHelper;
+        $this->clock = $clock;
     }
 
     /**
@@ -53,105 +50,112 @@ class EntityChangeLogger
      */
     public function onFlush(OnFlushEventArgs $event)
     {
-        $this->em = $event->getEntityManager();
-        $uow = $this->em->getUnitOfWork();
+        $em = $event->getEntityManager();
+        $uow = $em->getUnitOfWork();
+
+        if (!$user = $this->userHelper->getUser()) {
+            return;
+        }
+
+        if (!$user = $em->find('QL\Hal\Core\Entity\User', $user->getId())) {
+            return;
+        }
 
         // Entity Insertions
         foreach ($uow->getScheduledEntityInsertions() as $entity) {
-            $this->log($entity, self::ACTION_CREATE, $uow);
+            if ($log = $this->log($user, $entity, self::ACTION_CREATE)) {
+                $this->persist($em, $uow, $log);
+            }
         }
 
         // Entity Updates
         foreach ($uow->getScheduledEntityUpdates() as $entity) {
-            $this->log($entity, self::ACTION_UPDATE, $uow);
+            if ($log = $this->log($user, $entity, self::ACTION_UPDATE)) {
+                $this->addChangeset($log, $uow->getEntityChangeSet($entity));
+                $this->persist($em, $uow, $log);
+            }
         }
 
         // Entity Deletions
         foreach ($uow->getScheduledEntityDeletions() as $entity) {
-            $this->log($entity, self::ACTION_DELETE, $uow);
+            if ($log = $this->log($user, $entity, self::ACTION_DELETE)) {
+                $this->persist($em, $uow, $log);
+            }
         }
     }
 
     /**
-     * Prepare and persist a log entry
+     * Prepare an audit log from a changed entity.
      *
-     * @param $entity
-     * @param $action
-     * @param $uow
+     * @param User $user
+     * @param mixed $entity
+     * @param string $action
+     *
+     * @return AuditLog|null
      */
-    private function log($entity, $action, UnitOfWork $uow)
+    private function log(User $user, $entity, $action)
     {
         // prevent logging loop
-        if ($entity instanceof AuditLog || $entity instanceof Session || $entity instanceof User) {
+        if ($entity instanceof AuditLog || $entity instanceof User) {
             return;
         }
 
-        $reflect = new ReflectionClass($entity);
-
         // figure out the entity primary id
-        if ($action == self::ACTION_CREATE) {
-            $id = '?';
-        } else {
-            $id = (array)$uow->getEntityIdentifier($entity);
-            $id = reset($id);
+        $id = '?';
+        if (is_callable([$entity, 'getId']) && $entity->getId()) {
+            $id = $entity->getId();
         }
 
-        // figure out what data to show
-        if ($action == self::ACTION_DELETE) {
-            // deleted data (show state before delete)
-            $data = [];
-            foreach ($reflect->getProperties() as $property) {
-                $property->setAccessible(true);
-                $data[$property->getName()] = $property->getValue($entity);
-            }
-        } else {
-            // updated data (show changes only)
-            $data = $uow->getEntityChangeSet($entity);
-
-            // doctrine is weird
-            foreach ($data as $k => &$v) {
-                if (is_array($v) && count($v) === 2 && $v[0] === null) {
-                    $v = array_pop($v);
-                }
-            }
-        }
+        $fqcn = get_class($entity);
+        $classname = explode('\\', $fqcn);
+        $object = sprintf('%s:%s', array_pop($classname), $id);
 
         $log = new AuditLog;
-
-        $sessionUser = $this->userHelper->getUser();
-        $user = $this->em->find('QL\\Hal\\Core\\Entity\\User', $sessionUser->getId());
         $log->setUser($user);
-
-        $log->setRecorded($this->getTimepoint());
-        $log->setEntity(sprintf(
-            '%s:%s',
-            $reflect->getShortName(),
-            $id
-        ));
+        $log->setRecorded($this->clock->read());
+        $log->setEntity($object);
         $log->setAction($action);
-        $log->setData(json_encode($data));
+        $log->setData(json_encode($entity));
 
-        $this->em->persist($log);
-        $uow->computeChangeSet($this->em->getClassMetadata('QL\\Hal\\Core\\Entity\\AuditLog'), $log);
+        return $log;
     }
 
     /**
-     * Get a Timepoint representing the current time
+     * @param AuditLog $log
+     * @param array $changeset
      *
-     * @return Timepoint
+     * @return null
      */
-    private function getTimepoint()
+    private function addChangeset(AuditLog $log, array $changeset)
     {
-        $now = new DateTime('now', new DateTimeZone('UTC'));
+        $data = json_decode($log->getData(), true);
 
-        return new TimePoint(
-            $now->format('Y'),
-            $now->format('m'),
-            $now->format('d'),
-            $now->format('H'),
-            $now->format('i'),
-            $now->format('s'),
-            'UTC'
-        );
+        foreach ($changeset as $field => $properties) {
+            if (isset($data[$field])) {
+                $data[$field] = [
+                    'current' => $properties[0],
+                    'new' => $properties[1]
+                ];
+            }
+        }
+
+        $log->setData(json_encode($data));
+    }
+
+    /**
+     * Persist the audit log.
+     *
+     * @param EntityManager $em
+     * @param UnitOfWork $unit
+     * @param AuditLog $log
+     *
+     * @return null
+     */
+    private function persist(EntityManager $em, UnitOfWork $unit, AuditLog $log)
+    {
+        $em->persist($log);
+
+        $meta = $em->getClassMetadata('QL\Hal\Core\Entity\AuditLog');
+        $unit->computeChangeSet($meta, $log);
     }
 }
