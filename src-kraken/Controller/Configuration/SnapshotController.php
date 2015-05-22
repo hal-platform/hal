39934fa6fa7152826eb7ad8772ab92d0266dfdf3
Steve Kluck <stevekluck@quickenloans.com>
2015-05-22 15:47:26 -0400

    Add consul audit log to snapshot page

--- src-kraken/Controller/Configuration/SnapshotController.php
+++ src-kraken/Controller/Configuration/SnapshotController.php
@@ -13,9 +13,11 @@ use QL\Kraken\Entity\Application;
 use QL\Kraken\Entity\Configuration;
 use QL\Kraken\Entity\ConfigurationProperty;
 use QL\Kraken\Entity\Target;
+use QL\Kraken\Service\ConsulResponse;
 use QL\Kraken\Service\ConsulService;
 use QL\Panthor\ControllerInterface;
 use QL\Panthor\TemplateInterface;
+use QL\Panthor\Utility\Json;
 
 class SnapshotController implements ControllerInterface
 {
@@ -40,6 +42,11 @@ class SnapshotController implements ControllerInterface
     private $consul;
 
     /**
+     * @type Json
+     */
+    private $json;
+
+    /**
      * @type EntityRepository
      */
     private $targetRepo;
@@ -51,19 +58,23 @@ class SnapshotController implements ControllerInterface
      * @param Configuration $configuration
      * @param ConsulService $consul
      * @param EntityManagerInterface $em
+     * @param Json $json
      */
     public function __construct(
         TemplateInterface $template,
         Application $application,
         Configuration $configuration,
         ConsulService $consul,
-        EntityManagerInterface $em
+        EntityManagerInterface $em,
+        Json $json
     ) {
         $this->template = $template;
         $this->application = $application;
         $this->configuration = $configuration;
 
         $this->consul = $consul;
+        $this->json = $json;
+
         $this->targetRepo = $em->getRepository(Target::CLASS);
         $this->configurationPropertyRepo = $em->getRepository(ConfigurationProperty::CLASS);
     }
@@ -91,7 +102,8 @@ class SnapshotController implements ControllerInterface
             'target' => $target,
 
             'is_deployed' => $isDeployed,
-            'checksums' => $checksums
+            'checksums' => $checksums,
+            'audits' => $this->formatAuditLogs($this->configuration->audit())
         ];
 
         $this->template->render($context);
@@ -111,5 +123,47 @@ class SnapshotController implements ControllerInterface
         }
 
         return $checksums;
+    }
+
+    /**
+     * @param string|null $audits
+     *
+     * @return ConsulResponse[]
+     */
+    private function formatAuditLogs($audits)
+    {
+        if (!$audits) {
+            return [];
+        }
+
+        $decoded = $this->json->decode($audits);
+        if (!is_array($decoded)) {
+            return [];
+        }
+
+        $normalized = [];
+        foreach ($decoded as $log) {
+            if (!array_key_exists('key', $log)) continue;
+            if (!array_key_exists('type', $log)) continue;
+            if (!array_key_exists('detail', $log)) continue;
+
+            $normalized[] = (new ConsulResponse($log['key'], $log['type']))
+                ->withDetail($log['detail']);
+        }
+
+        usort($normalized, function($a, $b) {
+            if ($a->type() === $a->type()) {
+                // check key name
+                return strcasecmp($a->key(), $b->key());
+            }
+
+            if ($a->type() === 'update') {
+                return 1;
+            }
+
+            return -1;
+        });
+
+        return $normalized;
     }
 }
