2a0b2e477b118361bbb20b5750702f7a045591ac
Steve Kluck <stevekluck@quickenloans.com>
2015-07-22 08:49:17 -0400

    Dynamically load event logs on push and build pages

--- js/app/status/update-builds.js
+++ js/app/status/update-builds.js
@@ -2,6 +2,9 @@ var $ = require('jquery');
 var formatter = require('../util/time-formatter');
 var favico = require('favico');
 
+var nunjucks = require('nunjucks');
+var mplate1 = require('../../nunjucks-dist/eventlog.js');
+
 module.exports = {
     interval: 5,
     mode: 'table', // "table" for global build table, "build" for individual build status page
@@ -10,6 +13,12 @@ module.exports = {
     successClass: 'status-icon--success',
     failureClass: 'status-icon--error',
     buildTarget: '[data-build]',
+
+    logTable: '.js-event-logs',
+    logTarget: '[data-log]',
+
+    $logTable: null,
+    logs: {},
 
     favicon: null,
 
@@ -21,6 +30,19 @@ module.exports = {
                 animation: 'none',
                 fontStyle: 'bolder'
             });
+
+            // stub event logs, dont need to be updated further
+            $logTable = $(this.logTable);
+            if ($logTable.length > 0) {
+                this.$logTable = $logTable;
+
+                this.$logTable
+                    .find(this.logTarget)
+                    .each(function(index, item) {
+                        var id = $(item).data('log');
+                        _this.logs[id] = 'embedded';
+                    });
+            }
         }
 
         var $builds = $(this.buildTarget);
@@ -36,12 +58,12 @@ module.exports = {
                 _this.startUpdateTimer($item);
             }
         });
-
-        return $builds;
     },
     generateUrl: function(buildId, type) {
         if (type === 'api-update') {
             return '/api/builds/' + buildId;
+        } else if (type === 'logs') {
+            return '/api/builds/' + buildId + '/logs?embed=logs';
         }
     },
     checkStatus: function($elem) {
@@ -77,13 +99,96 @@ module.exports = {
                     .addClass(_this.failureClass);
             }
 
-            if (_this.mode == 'table') {
-                _this.updateTable(data, $elem);
-            } else {
+            if (_this.mode == 'build') {
                 _this.updateBuild(data, $elem);
+
+            } else if (_this.mode == 'table') {
+                _this.updateTable(data, $elem);
+            }
+
+            _this.handleThinkingLogs(currentStatus);
+        });
+
+        this.checkLogs(id);
+    },
+
+    checkLogs: function(id) {
+        if (this.$logTable === null) {
+            return;
+        }
+
+        var _this = this;
+        var logsEndpoint = this.generateUrl(id, 'logs');
+
+        // Requires these properties:
+        // - count
+        // - _embedded.logs
+        // - _embedded.logs[].id
+        // - _embedded.logs[].message
+        // - _embedded.logs[].event
+        // - _embedded.logs[].created
+        $.getJSON(logsEndpoint, function(data) {
+            if (data.count < 1) {
+                return;
+            }
+
+            if (!data.hasOwnProperty('_embedded')) {
+                return;
+            }
+
+            if (!data._embedded.hasOwnProperty('logs')) {
+                return;
+            }
+
+            var logs = data._embedded.logs,
+                hasNewLogs = false;
+
+            for(var index in logs) {
+                var log = logs[index];
+
+                if (typeof _this.logs[log.id] == 'undefined') {
+                    hasNewLogs = true;
+
+                    _this.logs[log.id] = log.message;
+                    _this.$logTable
+                        .append(_this.buildEventRow(log));
+                }
+            }
+
+            if (hasNewLogs) {
+                _this.$logTable
+                    .find('.js-empty-row')
+                    .remove().end()
+                    .find('.js-thinking-row')
+                    .appendTo(_this.$logTable);
             }
         });
     },
+
+    handleThinkingLogs: function(status) {
+        if (this.$logTable === null) {
+            return;
+        }
+
+        if (status == 'Waiting' || status == 'Building' || status == 'Pushing') {
+            var $thinking = this.$logTable
+                .find('.js-thinking-row');
+
+            // If thinking row already exists, just move it to the bottom
+            if ($thinking.length > 0) {
+                $thinking.appendTo(this.$logTable);
+            } else {
+                var $thinking = $('<tbody class="js-thinking-row">')
+                    .append('<tr><td><span class="status-icon--thinking">Loading...</span></td></tr>')
+                    .appendTo(this.$logTable);
+            }
+
+        } else {
+            this.$logTable
+                .find('.js-thinking-row').remove();
+        }
+    },
+
     startUpdateTimer: function($elem) {
         var _this = this;
 
@@ -176,6 +281,26 @@ module.exports = {
             formatted = formatter.formatDuration(duration);
         return '<time datetime="' + duration + '" title="' + formatted.absolute + '">' + formatted.relative + '</time>';
     },
+
+    buildEventRow: function(log) {
+
+        var eventRegex = /^(build|push).([a-z]*)$/i,
+            match = null,
+            logEvent = '';
+
+        match = eventRegex.exec(log.event);
+        if (match !== null && match.length > 0) {
+            logEvent = match.pop();
+        }
+
+        var context = {
+            log: log,
+            logEvent: logEvent,
+            logCreated: this.createTimeElement(log.created)
+        };
+
+        return nunjucks.render('eventlog.html', context);
+    }
     // formatDuration: function(start, end) {
     //     var start = moment(start),
     //         end = moment(end),
