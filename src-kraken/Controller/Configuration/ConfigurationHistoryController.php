5765c5a021c1d1f767a0689ba4004ad8f0916702
Steve Kluck <stevekluck@quickenloans.com>
2015-08-13 09:05:19 -0400

    Allow configuration history to be filtered

--- src-kraken/Controller/Configuration/ConfigurationHistoryController.php
+++ src-kraken/Controller/Configuration/ConfigurationHistoryController.php
@@ -12,15 +12,19 @@ use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\ORM\EntityRepository;
 use QL\Kraken\Core\Entity\Application;
 use QL\Kraken\Core\Entity\Configuration;
+use QL\Kraken\Core\Entity\Environment;
 use QL\Kraken\Core\Entity\Target;
 use QL\Kraken\Core\Repository\ConfigurationRepository;
 use QL\Panthor\Slim\NotFound;
 use QL\Panthor\ControllerInterface;
 use QL\Panthor\TemplateInterface;
+use Slim\Http\Request;
 
 class ConfigurationHistoryController implements ControllerInterface
 {
     const MAX_PER_PAGE = 25;
+
+    const REGEX_ENV = '/(?:environment|env|e):([a-zA-Z-]+)/';
 
     /**
      * @type TemplateInterface
@@ -41,6 +45,7 @@ class ConfigurationHistoryController implements ControllerInterface
      * @type EntityRepository
      */
     private $targetRepo;
+    private $environmentRepo;
 
     /**
      * @type ConfigurationRepository
@@ -60,8 +65,8 @@ class ConfigurationHistoryController implements ControllerInterface
     /**
      * @param TemplateInterface $template
      * @param Application $application
-     * @param Environment $environment
      * @param EntityManagerInterface $em
+     * @param Request $request
      * @param NotFound $notFound
      * @param array $parameters
      */
@@ -69,6 +74,7 @@ class ConfigurationHistoryController implements ControllerInterface
         TemplateInterface $template,
         Application $application,
         EntityManagerInterface $em,
+        Request $request,
         NotFound $notFound,
         array $parameters
     ) {
@@ -77,8 +83,10 @@ class ConfigurationHistoryController implements ControllerInterface
 
         $this->em = $em;
         $this->targetRepo = $em->getRepository(Target::CLASS);
+        $this->environmentRepo = $em->getRepository(Environment::CLASS);
         $this->configurationRepo = $em->getRepository(Configuration::CLASS);
 
+        $this->request = $request;
         $this->notFound = $notFound;
         $this->parameters = $parameters;
     }
@@ -89,15 +97,36 @@ class ConfigurationHistoryController implements ControllerInterface
     public function __invoke()
     {
         $page = (isset($this->parameters['page'])) ? $this->parameters['page'] : 1;
+        $searchFilter = is_string($this->request->get('search')) ? $this->request->get('search') : '';
 
         // 404, invalid page
         if ($page < 1) {
             return call_user_func($this->notFound);
         }
 
-        $configurations = $this->configurationRepo->getByApplication($this->application, self::MAX_PER_PAGE, ($page-1));
-        $deployed = $this->getDeployedConfigurations($this->application);
+        $environment = $this->getEnvironmentFromSearchFilter($searchFilter);
 
+        if ($environment) {
+            $configurations = $this->configurationRepo->getByApplicationForEnvironment(
+                $this->application,
+                $environment,
+                self::MAX_PER_PAGE,
+                ($page-1)
+            );
+
+        } elseif ($environment === false) {
+            // No env provided in search query
+            $configurations = $this->configurationRepo->getByApplication(
+                $this->application,
+                self::MAX_PER_PAGE,
+                ($page-1)
+            );
+
+        } else {
+            $configurations = [];
+        }
+
+        $deployed = ($configurations) ? $this->getDeployedConfigurations($this->application) : [];
         $total = count($configurations);
         $last = ceil($total / self::MAX_PER_PAGE);
 
@@ -107,7 +136,9 @@ class ConfigurationHistoryController implements ControllerInterface
 
             'application' => $this->application,
             'configurations' => $configurations,
-            'deployed' => $deployed
+            'deployed' => $deployed,
+
+            'search_filter' => $searchFilter
         ]);
     }
 
@@ -118,7 +149,7 @@ class ConfigurationHistoryController implements ControllerInterface
      *
      * @return array
      */
-    public function getDeployedConfigurations(Application $application)
+    private function getDeployedConfigurations(Application $application)
     {
         $deployed = [];
 
@@ -131,5 +162,20 @@ class ConfigurationHistoryController implements ControllerInterface
         }
 
         return $deployed;
+    }
+
+    /**
+     * @param string $search
+     *
+     * @return Environment|null|false
+     */
+    private function getEnvironmentFromSearchFilter($search)
+    {
+        if (preg_match(self::REGEX_ENV, $search, $matches) === 1) {
+            $name = strtolower(array_pop($matches));
+            return $this->environmentRepo->findOneBy(['name' => $name]);
+        }
+
+        return false;
     }
 }
