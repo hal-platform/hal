6f189a85c8ace3c53a09d9ab137195aeaab7925f
Steve Kluck <stevekluck@quickenloans.com>
2015-09-22 10:07:48 -0400

    Move tag sorting into trait

--- src/Controllers/Build/StartBuildController.php
+++ src/Controllers/Build/StartBuildController.php
@@ -12,12 +12,15 @@ use QL\Hal\Core\Entity\Application;
 use QL\Hal\Core\Entity\Environment;
 use QL\Hal\Core\Repository\EnvironmentRepository;
 use QL\Hal\Service\GitHubService;
+use QL\Hal\Utility\ReleaseSortingTrait;
 use QL\Panthor\ControllerInterface;
 use QL\Panthor\TemplateInterface;
 use Slim\Http\Request;
 
 class StartBuildController implements ControllerInterface
 {
+    use ReleaseSortingTrait;
+
     /**
      * @type TemplateInterface
      */
@@ -144,32 +147,7 @@ class StartBuildController implements ControllerInterface
     {
         $tags = $this->github->tags($application->githubOwner(), $application->githubRepo());
 
-       // $tags = [
-       //     ['name' => '3.1'],
-       //     ['name' => '3.0'],
-       //     ['name' => '3.1.2'],
-       //     ['name' => '3.1.1'],
-       //     ['name' => '4.0-rc1'],
-       //     ['name' => '1.0'],
-       //     ['name' => '4.5'],
-       //     ['name' => '2'],
-       //     ['name' => '4.0-rc2'],
-       //     ['name' => '2.5'],
-       //     ['name' => '4.0-alpha2'],
-       //     ['name' => '2.1.2'],
-       //     ['name' => '4.0-beta1'],
-       //     ['name' => '4.0-alpha1'],
-       //     ['name' => 'a'],
-       //     ['name' => 'c'],
-       //     ['name' => 'v4.5.1'],
-       //     ['name' => 'v3.1'],
-       //     ['name' => 'b'],
-       //     ['name' => '2.1.1.1'],
-       //     ['name' => '14.1'],
-       //     ['name' => '4.0'],
-       // ];
-
-        usort($tags, $this->tagSorter());
+        usort($tags, $this->releaseSorter());
 
         return $tags;
     }
@@ -194,64 +172,5 @@ class StartBuildController implements ControllerInterface
         });
 
         return $pr;
-    }
-
-    /**
-     * @return callable
-     */
-    public function tagSorter()
-    {
-        return function($a, $b) {
-            $matchesA = $this->semver($a['name']);
-            $matchesB = $this->semver($b['name']);
-
-            // If both are not semver, bump non-semver to bottom
-            if ($matchesA === false xor $matchesB === false) {
-                if ($matchesA === false) {
-                    return 1;
-                }
-
-                return -1;
-            }
-
-            // both non-semver
-            if ($matchesA === false && $matchesB === false) {
-                return strcasecmp($a['name'], $b['name']);
-            }
-
-            // both sem-ver
-
-            // special case for when version number is the same (1.0-beta1 vs 1.0-alpha1)
-            if ($matchesA[1] == $matchesB[1]) {
-                $textA = (isset($matchesA[5])) ? $matchesA[5] : '';
-                $textB = (isset($matchesB[5])) ? $matchesB[5] : '';
-                $numA = (isset($matchesA[6])) ? $matchesA[6] : 0;
-                $numB = (isset($matchesB[6])) ? $matchesB[6] : 0;
-
-                // special case when release type is the same (1.0-rc1 vs 1.0-rc2)
-                if ($textA == $textB) {
-                    return strcasecmp($numB, $numA);
-                }
-
-                return strcasecmp($textB, $textA);
-            }
-
-            return version_compare($matchesB[1], $matchesA[1]);
-        };
-    }
-
-    /**
-     * Check to see if a string is *close* to being valid semver. Checking for actual spec compliance turns out to be
-     * too restrictive in most cases and won't match what users will expect it to.
-     *
-     * For example, this will catch things like 1.0, 1, 1.0-rc1, 1.0.0.0, and v1.0 despite those values not being
-     * compliant with the semver spec.
-     *
-     * @param $value
-     * @return false|string
-     */
-    private function semver($value)
-    {
-        return (preg_match('#^v?(([0-9]+\.?)+)(-(([a-zA-Z]+)([0-9]+)))?#', $value, $matches) > 0) ? $matches : false;
     }
 }
---
+++ src/Utility/ReleaseSortingTrait.php
@@ -0,0 +1,71 @@
+<?php
+/**
+ * @copyright ©2015 Quicken Loans Inc. All rights reserved. Trade Secret,
+ *    Confidential and Proprietary. Any dissemination outside of Quicken Loans
+ *    is strictly prohibited.
+ */
+
+namespace QL\Hal\Utility;
+
+trait ReleaseSortingTrait
+{
+    /**
+     * @return callable
+     */
+    public function releaseSorter()
+    {
+        return function($a, $b) {
+            $matchesA = $this->semver($a['name']);
+            $matchesB = $this->semver($b['name']);
+
+            // If both are not semver, bump non-semver to bottom
+            if ($matchesA === false xor $matchesB === false) {
+                if ($matchesA === false) {
+                    return 1;
+                }
+
+                return -1;
+            }
+
+            // both non-semver
+            if ($matchesA === false && $matchesB === false) {
+                return strcasecmp($a['name'], $b['name']);
+            }
+
+            // both sem-ver
+
+            // special case for when version number is the same (1.0-beta1 vs 1.0-alpha1)
+            if ($matchesA[1] == $matchesB[1]) {
+                $textA = (isset($matchesA[5])) ? $matchesA[5] : '';
+                $textB = (isset($matchesB[5])) ? $matchesB[5] : '';
+                $numA = (isset($matchesA[6])) ? $matchesA[6] : 0;
+                $numB = (isset($matchesB[6])) ? $matchesB[6] : 0;
+
+                // special case when release type is the same (1.0-rc1 vs 1.0-rc2)
+                if ($textA == $textB) {
+                    return strcasecmp($numB, $numA);
+                }
+
+                return strcasecmp($textB, $textA);
+            }
+
+            return version_compare($matchesB[1], $matchesA[1]);
+        };
+    }
+
+    /**
+     * Check to see if a string is *close* to being valid semver. Checking for actual spec compliance turns out to be
+     * too restrictive in most cases and won't match what users will expect it to.
+     *
+     * For example, this will catch things like 1.0, 1, 1.0-rc1, 1.0.0.0, and v1.0 despite those values not being
+     * compliant with the semver spec.
+     *
+     * @param $value
+     *
+     * @return false|string
+     */
+    private function semver($value)
+    {
+        return (preg_match('#^v?(([0-9]+\.?)+)(-(([a-zA-Z]+)([0-9]+)))?#', $value, $matches) > 0) ? $matches : false;
+    }
+}
