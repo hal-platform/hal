c3d569f1934e5f5569a15565acff3df7c77eccf4
Steve Kluck <stevekluck@quickenloans.com>
2015-12-14 14:50:50 -0500

    Allow admins to disable/enable users
    Fix #169

--- src/Controllers/User/DisableUserHandler.php
+++ src/Controllers/User/DisableUserHandler.php
@@ -10,16 +10,23 @@ namespace QL\Hal\Controllers\User;
 use Doctrine\ORM\EntityManagerInterface;
 use QL\Hal\Core\Entity\User;
 use QL\Hal\Flasher;
+use QL\Hal\Service\PermissionService;
 use QL\Panthor\ControllerInterface;
 
 class DisableUserHandler implements ControllerInterface
 {
     const SUCCESS = 'User Disabled.';
+    const ERR_THANKS_FOR_ASKING = 'HAL Administrators cannot be disabled.';
 
     /**
      * @type EntityManagerInterface
      */
     private $em;
+
+    /**
+     * @type PermissionService
+     */
+    private $permissions;
 
     /**
      * @type Flasher
@@ -33,15 +40,19 @@ class DisableUserHandler implements ControllerInterface
 
     /**
      * @param EntityManagerInterface $em
+     * @param PermissionService $permissions
      * @param Flasher $flasher
      * @param User $selectedUser
      */
     public function __construct(
         EntityManagerInterface $em,
+        PermissionService $permissions,
         Flasher $flasher,
         User $selectedUser
     ) {
         $this->em = $em;
+        $this->permissions = $permissions;
+
         $this->flasher = $flasher;
         $this->selectedUser = $selectedUser;
     }
@@ -51,6 +62,14 @@ class DisableUserHandler implements ControllerInterface
      */
     public function __invoke()
     {
+        $permissions = $this->permissions->getUserPermissions($this->selectedUser);
+
+        if ($permissions->isSuper()) {
+            $this->flasher
+                ->withFlash(self::ERR_THANKS_FOR_ASKING, 'error', ' Remove this level of permission before disabling this user.')
+                ->load('user', ['user' => $this->selectedUser->id()]);
+        }
+
         $this->selectedUser
             ->withIsActive(false);
 
