afb099ee138eb46db3499a06945bec29576e61b6
Steve Kluck <stevekluck@quickenloans.com>
2015-12-22 10:02:20 -0500

    Move js dependencies from bower to npm

--- js/webpack.config.js
+++ js/webpack.config.js
@@ -3,7 +3,8 @@ var CommonsChunkPlugin = webpack.optimize.CommonsChunkPlugin;
 var UglifyJsPlugin = webpack.optimize.UglifyJsPlugin;
 var ProvidePlugin = webpack.ProvidePlugin;
 
-var vendor = __dirname + '/../bower_components';
+var vendorOld = __dirname + '/../bower_components';
+var vendor = __dirname + '/../node_modules';
 
 module.exports = {
     context: __dirname,
@@ -13,8 +14,9 @@ module.exports = {
             'favico',
             'jquery',
             'jquery.searchable',
-            'jquery.sortable',
             'jquery.tablesaw',
+            'jquery.tablesaw.init',
+            'jquery.hideseek',
 
             'nunjucks',
             'sugarjs-date',
@@ -26,16 +28,17 @@ module.exports = {
     resolve: {
         alias: {
 
-            'favico':               vendor + '/favico.js/favico.js',
-            'jquery':               vendor + '/jquery/dist/jquery.js',
-            'jquery.searchable':    vendor + '/jquery-searchable/jquery.searchable.js',
-            'jquery.sortable':      vendor + '/html5sortable/jquery.sortable.js',
-            'jquery.tablesaw':      vendor + '/tablesaw/dist/stackonly/tablesaw.stackonly.js',
+            'favico':                  vendor + '/favico.js/favico.js',
+            'jquery':                  vendor + '/jquery/dist/jquery.js',
+            'jquery.searchable':    vendorOld + '/jquery-searchable/jquery.searchable.js',
+            'jquery.tablesaw':         vendor + '/tablesaw/dist/stackonly/tablesaw.stackonly.js',
+            'jquery.tablesaw.init':    vendor + '/tablesaw/dist/tablesaw-init.js',
+            'jquery.hideseek':         vendor + '/hideseek/jquery.hideseek.js',
 
-            'nunjucks':             vendor + '/nunjucks/browser/nunjucks.js',
-            'sugarjs-date':         vendor + '/sugarjs-date/sugar-date.js',
-            'svg4everybody':        vendor + '/svg4everybody/svg4everybody.js',
-            'typed':                vendor + '/typed.js/js/typed.js'
+            'nunjucks':                vendor + '/nunjucks/browser/nunjucks.js',
+            'sugarjs-date':            vendor + '/sugar-date/sugar-date.js',
+            'svg4everybody':           vendor + '/svg4everybody/dist/svg4everybody.js',
+            'typed':                vendorOld + '/typed.js/js/typed.js'
         }
     },
     output: {
