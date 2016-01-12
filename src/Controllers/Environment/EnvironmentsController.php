a098688569ea507a7cd4e6433e6391d8d79f8741
Steve Kluck <stevekluck@quickenloans.com>
2016-01-12 14:59:53 -0500

    Update copyright block for 2016

--- src/Controllers/Environment/EnvironmentsController.php
+++ src/Controllers/Environment/EnvironmentsController.php
@@ -1,8 +1,8 @@
 <?php
 /**
- * @copyright ©2014 Quicken Loans Inc. All rights reserved. Trade Secret,
- *    Confidential and Proprietary. Any dissemination outside of Quicken Loans
- *    is strictly prohibited.
+ * @copyright (c) 2016 Quicken Loans Inc.
+ *
+ * For full license information, please view the LICENSE distributed with this source code.
  */
 
 namespace QL\Hal\Controllers\Environment;
