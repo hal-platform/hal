99b603201cd069adab4bca73bcd42e34f1e2631e
Steve Kluck <stevekluck@quickenloans.com>
2017-01-09 09:11:50 -0500

    Update list of aws regions

--- src/Validator/ServerValidator.php
+++ src/Validator/ServerValidator.php
@@ -53,15 +53,25 @@ class ServerValidator
      */
     private static $awsRegions = [
         'ap-northeast-1',
-        'ap-southeast-2',
+        'ap-northeast-2',
         'ap-southeast-1',
+        'ap-southeast-2',
+        'ap-south-1',
+
+        'ca-central-1',
+
         'cn-north-1',
+
         'eu-central-1',
         'eu-west-1',
-        'us-east-1',
-        'us-west-1',
-        'us-west-2',
+        'eu-west-2',
+
         'sa-east-1',
+
+        'us-east-1',
+        'us-east-2',
+        'us-west-1',
+        'us-west-2'
     ];
 
     /**
