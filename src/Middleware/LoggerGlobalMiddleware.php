44f0954386d50837027198b8b75a0315f456dd57
Steve Kluck <stevekluck@quickenloans.com>
2017-02-20 12:36:09 -0500

    Rewrite logger middleware for slim 3

---
+++ src/Middleware/LoggerGlobalMiddleware.php
@@ -0,0 +1,163 @@
+<?php
+/**
+ * @copyright (c) 2017 Quicken Loans Inc.
+ *
+ * For full license information, please view the LICENSE distributed with this source code.
+ */
+
+namespace Hal\UI\Middleware;
+
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use QL\MCP\Common\IPv4Address;
+use QL\MCP\Logger\MessageInterface;
+use QL\MCP\Logger\MessageFactoryInterface;
+use QL\Panthor\MiddlewareInterface;
+
+/**
+ * This middleware requires "ql/mcp-logger" >= 3.0
+ *
+ * Sets the following default log message properties:
+ * - serverHostname
+ * - serverIP
+ * - requestMethod
+ * - requestURL
+ * - context
+ *     - referrer
+ * - userAgent
+ * - userIP
+ *
+ * You must manually set the following in your configuration:
+ * - serverEnvironment
+ * - applicationID
+ *  ```
+ */
+class LoggerGlobalMiddleware implements MiddlewareInterface
+{
+    /**
+     * @var MessageFactoryInterface
+     */
+    private $factory;
+
+    /**
+     * Trusted headers for client IP sources
+     *
+     * @var array
+     */
+    private $headers = [
+        'Forwarded',
+        'Forwarded-For',
+        'Client-Ip',
+        'X-Forwarded',
+        'X-Forwarded-For',
+        'X-Cluster-Client-Ip',
+    ];
+
+    /**
+     * @param MessageFactoryInterface $factory
+     */
+    public function __construct(MessageFactoryInterface $factory)
+    {
+        $this->factory = $factory;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function __invoke(ServerRequestInterface $request, ResponseInterface $response, callable $next)
+    {
+        // server
+        $this->factory->setDefaultProperty(MessageInterface::SERVER_HOSTNAME, gethostname());
+
+        if ($serverIP = IPv4Address::create($this->getServerIP($request))) {
+            $this->factory->setDefaultProperty(MessageInterface::SERVER_IP, $serverIP);
+        }
+
+        // request
+        $this->factory->setDefaultProperty(MessageInterface::REQUEST_METHOD,  $request->getMethod());
+
+        $this->factory->setDefaultProperty(MessageInterface::REQUEST_URL,  (string) $request->getUri());
+
+        // client
+        if ($referer = $this->getFirstHeader($request, 'referer')) {
+            $this->factory->setDefaultProperty('referrer', $referer);
+        }
+
+        if ($agent = $this->getFirstHeader($request, 'user-agent')) {
+            $this->factory->setDefaultProperty(MessageInterface::USER_AGENT, $agent);
+        }
+
+        if ($userIP = $this->getClientIP($request)) {
+            if ($userIP = IPv4Address::create(reset($userIP))) {
+                $this->factory->setDefaultProperty(MessageInterface::USER_IP, $userIP);
+            }
+        }
+
+        return $next($request, $response);
+    }
+
+    /**
+     * @see https://github.com/oscarotero/psr7-middlewares/blob/master/src/Middleware/ClientIp.php
+     *
+     * @param ServerRequestInterface $request
+     *
+     * @return string[]
+     */
+    private function getClientIP(ServerRequestInterface $request)
+    {
+        $server = $request->getServerParams();
+        $ips = [];
+
+        if (!empty($server['REMOTE_ADDR']) && filter_var($server['REMOTE_ADDR'], FILTER_VALIDATE_IP)) {
+            $ips[] = $server['REMOTE_ADDR'];
+        }
+
+        foreach ($this->headers as $name) {
+            $header = $request->getHeaderLine($name);
+            if (!empty($header)) {
+                foreach (array_map('trim', explode(',', $header)) as $ip) {
+                    if ((array_search($ip, $ips) === false) && filter_var($ip, FILTER_VALIDATE_IP)) {
+                        $ips[] = $ip;
+                    }
+                }
+            }
+        }
+
+        return $ips;
+    }
+
+    /**
+     * @param ServerRequestInterface $request
+     *
+     * @return string
+     */
+    private function getServerIP(ServerRequestInterface $request)
+    {
+        $server = $request->getServerParams();
+
+        if (!empty($server['SERVER_ADDR']) && filter_var($server['SERVER_ADDR'], FILTER_VALIDATE_IP)) {
+            return $server['SERVER_ADDR'];
+        }
+
+        return '';
+    }
+
+    /**
+     * @param ServerRequestInterface $request
+     * @param string $header
+     *
+     * @return string
+     */
+    private function getFirstHeader(ServerRequestInterface $request, $header)
+    {
+        if (!$values = $request->getHeader($header)) {
+            return '';
+        }
+
+        if (!$value = reset($values)) {
+            return '';
+        }
+
+        return $value;
+    }
+}
