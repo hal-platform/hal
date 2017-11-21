801b7742a0f0b201b9b8b0eb81ba8624b935f6aa
Byron Nagi <byronnagi@quickenloans.com>
2017-11-21 10:30:12 -0500

    missed rollback page

--- src/Controllers/Release/RollbackController.php
+++ src/Controllers/Release/RollbackController.php
@@ -8,14 +8,14 @@
 namespace Hal\UI\Controllers\Release;
 
 use Doctrine\ORM\EntityManagerInterface;
+use Hal\Core\Entity\Application;
+use Hal\Core\Entity\Release;
+use Hal\Core\Entity\Target;
+use Hal\Core\Repository\ReleaseRepository;
 use Hal\UI\Controllers\PaginationTrait;
 use Hal\UI\Controllers\TemplatedControllerTrait;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
-use QL\Hal\Core\Entity\Application;
-use QL\Hal\Core\Entity\Deployment;
-use QL\Hal\Core\Entity\Push;
-use QL\Hal\Core\Repository\PushRepository;
 use QL\Panthor\ControllerInterface;
 use QL\Panthor\TemplateInterface;
 
@@ -32,9 +32,9 @@ class RollbackController implements ControllerInterface
     private $template;
 
     /**
-     * @var PushRepository
+     * @var ReleaseRepository
      */
-    private $pushRepo;
+    private $releaseRepository;
 
     /**
      * @var callable
@@ -49,7 +49,7 @@ class RollbackController implements ControllerInterface
     public function __construct(TemplateInterface $template, EntityManagerInterface $em, callable $notFound)
     {
         $this->template = $template;
-        $this->pushRepo = $em->getRepository(Push::class);
+        $this->releaseRepository = $em->getRepository(Release::class);
 
         $this->notFound = $notFound;
     }
@@ -60,16 +60,16 @@ class RollbackController implements ControllerInterface
     public function __invoke(ServerRequestInterface $request, ResponseInterface $response)
     {
         $application = $request->getAttribute(Application::class);
-        $deployment = $request->getAttribute(Deployment::class);
+        $target = $request->getAttribute(Target::class);
 
         $page = $this->getCurrentPage($request);
         if ($page === null) {
             return ($this->notFound)($request, $response);
         }
 
-        $pushes = $this->pushRepo->getByDeployment($deployment, self::MAX_PER_PAGE, ($page-1));
+        $releases = $this->releaseRepository->getByTarget($target, self::MAX_PER_PAGE, ($page - 1));
 
-        $total = count($pushes);
+        $total = count($releases);
         $last = ceil($total / self::MAX_PER_PAGE);
 
         return $this->withTemplate($request, $response, $this->template, [
@@ -77,8 +77,8 @@ class RollbackController implements ControllerInterface
             'last' => $last,
 
             'application' => $application,
-            'deployment' => $deployment,
-            'pushes' => $pushes
+            'target' => $target,
+            'releases' => $releases
         ]);
     }
 }
