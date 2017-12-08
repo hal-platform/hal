1b81825ca5c772d998f6c6fbf4008c4a19df118a
Thomason, James <JamesThomason@quickenloans.com>
2017-12-08 14:14:08 -0500

    Add paged list of all pushes (all apps)

---
+++ src/Controllers/Release/ReleasesListController.php
@@ -0,0 +1,78 @@
+<?php
+/**
+ * @copyright (c) 2016 Quicken Loans Inc.
+ *
+ * For full license information, please view the LICENSE distributed with this source code.
+ */
+
+namespace Hal\UI\Controllers\Release;
+
+use Doctrine\ORM\EntityManagerInterface;
+use Hal\Core\Entity\Release;
+use Hal\Core\Repository\ReleaseRepository;
+use Hal\UI\Controllers\PaginationTrait;
+use Hal\UI\Controllers\TemplatedControllerTrait;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use QL\Panthor\ControllerInterface;
+use QL\Panthor\TemplateInterface;
+
+class ReleasesListController implements ControllerInterface
+{
+    use PaginationTrait;
+    use TemplatedControllerTrait;
+
+    private const MAX_PER_PAGE = 25;
+
+    /**
+     * @var TemplateInterface
+     */
+    private $template;
+
+    /**
+     * @var ReleaseRepository
+     */
+    private $releaseRepo;
+
+    /**
+     * @var callable
+     */
+    private $notFound;
+
+    /**
+     * @param TemplateInterface $template
+     * @param EntityManagerInterface $em
+     * @param callable $notFound
+     */
+    public function __construct(TemplateInterface $template, EntityManagerInterface $em, callable $notFound)
+    {
+        $this->template = $template;
+        $this->releaseRepo = $em->getRepository(Release::class);
+
+        $this->notFound = $notFound;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function __invoke(ServerRequestInterface $request, ResponseInterface $response)
+    {
+        $page = $this->getCurrentPage($request);
+        if ($page === null) {
+            return ($this->notFound)($request, $response);
+        }
+
+        $releases = $this->releaseRepo->findBy([], ['created' => 'DESC'], self::MAX_PER_PAGE, ($page - 1));
+
+        $total = count($releases);
+        $last = ceil($total / self::MAX_PER_PAGE);
+
+        return $this->withTemplate($request, $response, $this->template, [
+            'page' => $page,
+            'last' => $last,
+
+            'releases' => $releases
+        ]);
+    }
+}
+
