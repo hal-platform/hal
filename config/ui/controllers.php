3a7646c5f175ded216e0862dd5e605e3fd2ff604
Steve Kluck <steve@kluck.engineering>
2018-06-06 22:40:40 -0400

    Inline twig templates under ui controllers

--- config/ui/controllers.php
+++ config/ui/controllers.php
@@ -14,11 +14,47 @@ return function (ContainerConfigurator $container) {
 
     $s
         ('home.page', \Hal\UI\Controllers\HomeController::class)
-            ->arg('$template', ref('home.twig'))
+            ->arg('$template', twig('home.twig'))
+
+        ('maintenance.page', \Hal\UI\Controllers\StaticController::class)
+            ->arg('$template', twig('error.maintenance.twig'))
+        ('denied.page', \Hal\UI\Controllers\StaticController::class)
+            ->arg('$template', twig('error.denied.twig'))
     ;
 
     $s
-        ('home.twig', LazyTwig::class)
-            ->arg('$template', 'home.twig')
+        ('latest_release.page', \Hal\UI\Controllers\StaticController::class)
+            ->arg('$template', twig('latest_release.twig'))
+        ('styleguide.page', \Hal\UI\Controllers\StaticController::class)
+            ->arg('$template', twig('styleguide.html.twig'))
+        ('styleguide.icons.page', \Hal\UI\Controllers\StaticController::class)
+            ->arg('$template', twig('styleguide.icons.html.twig'))
+
+        ('help.page', \Hal\UI\Controllers\StaticController::class)
+            ->arg('$template', twig('help/help.twig'))
+        ('help.scripting.page', \Hal\UI\Controllers\StaticController::class)
+            ->arg('$template', twig('help/scripting.twig'))
+        ('help.application_setup.page', \Hal\UI\Controllers\StaticController::class)
+            ->arg('$template', twig('help/application_setup.twig'))
+    ;
+
+    $s
+        ('signin.page', \Hal\UI\Controllers\Auth\SignInController::class)
+            ->arg('$template', twig('auth/signin.twig'))
+            ->arg('$cookies', ref('cookie.handler'))
+
+        ('signin.middleware',      \Hal\UI\Controllers\Auth\SignInHandler::class)
+        ('signout.handler',        \Hal\UI\Controllers\Auth\SignOutHandler::class)
+        ('signout.oauth2_handler', \Hal\UI\Controllers\Auth\SignInCallbackHandler::class)
+            ->arg('$cookies', ref('cookie.handler'))
+
+        ('signin.setup.page', \Hal\UI\Controllers\Auth\SignInSetupController::class)
+            ->arg('$template', twig('auth/signin_setup.twig'))
     ;
 };
+
+function twig($template) {
+    return inline(LazyTwig::class)
+        ->arg('$template', $template)
+        ->autowire();
+}
